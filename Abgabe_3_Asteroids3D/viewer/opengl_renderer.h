e8889149f242df4f7c0d583bf79dafaeb8997a66
Julytumb <jule_rieger@yahoo.de>
2026-01-20 22:46:23 +0100

    Revert "A3_setup"
    This reverts commit ddcf9a41441c867fbf4efef40a85a88bfc374f32.

--- Abgabe_3_Asteroids3D/viewer/opengl_renderer.h
+++ Abgabe_3_Asteroids3D/viewer/opengl_renderer.h
@@ -10,7 +10,6 @@
 #include "game.h"
 #include "renderer.h"
 #include "debug.h"
-#include "wavefront.h"
 #include <array>
 #include <vector>
 #include <memory>
@@ -92,12 +91,7 @@ public:
   virtual void render();
   
   virtual void exit(); 
-
-private:
-  WavefrontObject spaceship_model_;
-  WavefrontObject asteroid_model_;
-  WavefrontObject torpedo_model_;
-  WavefrontObject saucer_model_;
+  
 };
 
 #endif
