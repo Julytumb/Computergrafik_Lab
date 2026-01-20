87fbd08138c51282562b8ee3eb179c20021bb427
Julytumb <jule_rieger@yahoo.de>
2026-01-20 13:29:16 +0100

    Abgabe3_Aufg_1+Aufg_2_Setup

---
+++ Abgabe_3_AsteroidsOpenGL/asteroids_opengl/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/julytumb/projects/Computergrafik_Lab/Abgabe_3_AsteroidsOpenGL/asteroids_opengl
+# Build directory: /home/julytumb/projects/Computergrafik_Lab/Abgabe_3_AsteroidsOpenGL/asteroids_opengl
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Abgabe_3_Matrix/matrix/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/julytumb/projects/Computergrafik_Lab/Abgabe_3/matrix
+# Build directory: /home/julytumb/projects/Computergrafik_Lab/Abgabe_3/matrix
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
